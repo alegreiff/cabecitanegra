3670fda0040911e00b2bfd4b2a90dc0fc138e61c
Jaime de Greiff <alegreiff@gmail.com>
2023-09-03 11:28:06 -0500

    Somos

--- src/app/cabecita/inicio/inicio.component.ts
+++ src/app/cabecita/inicio/inicio.component.ts
@@ -1,32 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { DatosService } from '../datos.service';
+import { Component, OnInit } from "@angular/core";
+import { DatosService } from "../datos.service";
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  selector: "app-inicio",
+  templateUrl: "./inicio.component.html",
+  styleUrls: ["./inicio.component.scss"],
 })
 export class InicioComponent implements OnInit {
-
-  constructor(private ds: DatosService) { }
+  constructor(private ds: DatosService) {}
   lang: string;
-  title = 'cabecita';
-  contenidoES: string=`Somos una empresa colombiana de <strong>creación</strong> y <strong>producción</strong> de contenidos para cine y televisión fundada en 2009 por Carolina Mosquera y Alfonso Acosta, realizadores de cine y televisión y docentes. Nos interesa desarrollar y producir proyectos de alto valor artístico en los que se asuman riesgos narrativos y estéticos, historias con visiones fuertes, personales y originales. 
+  title = "cabecita";
+  contenidoES: string = `Si Somos una empresa colombiana de <strong>creación</strong> y <strong>producción</strong> de contenidos para cine y televisión fundada en 2009 por Carolina Mosquera y Alfonso Acosta, realizadores de cine y televisión y docentes. Nos interesa desarrollar y producir proyectos de alto valor artístico en los que se asuman riesgos narrativos y estéticos, historias con visiones fuertes, personales y originales. 
    
   <br><br>
-  En Cabecitanegra encontrarán un espacio íntimo de creación, de aprendizaje, y de trabajo colaborativo.`
+  En Cabecitanegra encontrarán un espacio íntimo de creación, de aprendizaje, y de trabajo colaborativo.`;
   contenidoEN: string = `Independent film production company based in Bogotá, Colombia interested in producing art-house content. Its films and TV series are caracterized by its multiple reading layers with a strong, fresh and original vision.
   <br><br>
-  A creative look on production processes, the strength of good ideas, the passion and a strong authorial character, guide the production of our projects.`
-//contenido: string;
-ngOnInit(){
-  //this.contenido = this.contenidoES;
-  this.ds.lang.subscribe( (val: string) =>  this.lang = val)
-}
+  A creative look on production processes, the strength of good ideas, the passion and a strong authorial character, guide the production of our projects.`;
+  //contenido: string;
+  ngOnInit() {
+    //this.contenido = this.contenidoES;
+    this.ds.lang.subscribe((val: string) => (this.lang = val));
+  }
 
-
-
-/* idiomaCont(lang: string){
+  /* idiomaCont(lang: string){
 if(lang === 'es'){
   this.contenido=this.contenidoES;
 }
@@ -35,6 +32,4 @@ if(lang=== 'en'){
   this.contenido=this.contenidoEN;
 }
 } */
-
-
 }
